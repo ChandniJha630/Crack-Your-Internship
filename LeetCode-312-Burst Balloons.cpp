b7dd2b65ce0470de39b4be03e431c9f9a3f8ee95
CHANDNI JHA <96429544+ChandniJha630@users.noreply.github.com>
2023-12-11 12:33:55 +0530

    Update LeetCode-312-Burst Balloons.cpp

--- LeetCode-312-Burst Balloons.cpp
+++ LeetCode-312-Burst Balloons.cpp
@@ -24,3 +24,48 @@ n == nums.length
 0 <= nums[i] <= 100
 
 */
+
+#include <vector>
+#include <climits>
+
+class Solution {
+    
+    int solve(vector<int>& arr, int start_index, int end_index, vector<vector<int>>& dp){
+        if(start_index > end_index) return 0;
+        if(dp[start_index][end_index] != -1) return dp[start_index][end_index];
+
+        int ans = 0;
+        for(int k = start_index; k <= end_index; k++){
+            int left_value = (start_index <= k - 1) ? solve(arr, start_index, k-1, dp) : 0;
+            int right_value = (k + 1 <= end_index) ? solve(arr, k+1, end_index, dp) : 0;
+            
+            int temp_ans = left_value + right_value + arr[start_index-1] * arr[k] * arr[end_index+1];
+            ans = max(ans, temp_ans);
+        }
+        dp[start_index][end_index] = ans;
+        return ans;
+    }
+
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size() + 2;
+        vector<int> arr(n, 1);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        for(int i = 1; i < n-1; i++){
+            arr[i] = nums[i-1];
+        }
+
+        return solve(arr, 1, n-2, dp);
+    }
+};
+int main() {
+    Solution sol;
+    std::vector<int> nums = {3, 1, 5, 8};
+    
+    // Call the maxCoins function and print the result
+    int result = sol.maxCoins(nums);
+    std::cout << "Maximum coins: " << result << std::endl;
+
+    return 0;
+}
